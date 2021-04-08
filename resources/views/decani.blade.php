459d000e3c6e29bfc4e40e468e5c5338b11a29cb
unknown <egzonhajdinnii@gmail.com>
2021-04-08 13:05:13 +0200

    Qytetet & Modalet

---
+++ resources/views/decani.blade.php
@@ -0,0 +1,127 @@
+@extends('layouts.app')
+
+@section('page_name', __('Decani'))
+
+@section('content')
+    <!-- Modal -->
+
+  <style type="text/css">
+    .modali {
+      border-style: solid;
+      border-width: 1px;
+      background-color: white;
+    }
+  </style>
+    
+      <div class="row" style=" padding: 12px;">
+      	
+  <div class="col-md-6"><h3 class="modal-title" id="exampleModalCenterTitle" style="align-items: center;">Deçan</h3></div>
+  <div class="col-md-6" style="margin: 15px 0px 0px"><form action="pages-search-results.html" class="search nav-form">
+            <div class="input-group">
+                <input type="text" class="form-control" name="q" id="q" placeholder="Search...">
+                <span class="input-group-append">
+								<button class="btn btn-default" type="submit"><i class="fas fa-search"></i></button>
+							</span>
+            </div>
+        </form>
+</div>
+        
+        
+    
+      </div>
+      <div class="modal-body">
+
+      	<div class="row" style="padding: 10px;">
+  <div class="col-md-4 modali"><strong><h3 style="color: black;">Kolegjiumi</h3></strong></div>
+  <div class="col-md-4 modali"><strong><h3 style="color: black;">Zyrtari Komunal</h3></strong></div>
+  <div class="col-md-4 modali"><strong><h3 style="color: black;">Informata</h3></strong></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px; "" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px; "data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div><div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div><div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><h4>test</h4></div>
+  <div class="col-md-4 modali"><button type="button" class="btn btn-info" style="margin-top: 4px;" data-toggle="modal" data-target="#Deçan">me shume info</button></div>
+</div>
+
+
+        <!-- Emri Mbiemri: <b>Bashkim Ramosaj</b><br>Nr i telefonit: <b>0390/361-100</b>
+          	<br>Nr i fax: <b>0390/361-785</b><br>Emaili: <b>bashkim.ramosaj@rks-gov.net</b> -->
+      </div>
+
+
+      <div class="modal fade" id="Deçan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="exampleModalCenterTitle" style="align-items: center;">Deçan</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Emri Mbiemri: <b>Bashkim Ramosaj</b><br>Nr i telefonit: <b>0390/361-100</b>
+            <br>Nr i fax: <b>0390/361-785</b><br>Emaili: <b>bashkim.ramosaj@rks-gov.net</b>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
+    </div>
+  </div>
+</div>
+      
+    
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,8 @@ Route::group(['middleware' => 'auth'], function(){
 
 });
 Auth::routes();
+
+
+Route::get('/decani', function() {
+	return view ('decani');
+});
